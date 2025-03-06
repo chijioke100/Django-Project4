d2d1b8999fad55bbc9430f47abff52b56a13f076
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2025-03-06 15:20:13 +0000

    create superuser

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from pathlib import Path
 import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-py05&e2f0tp9i8ievb_n!kbrrs55u+m171bw!yf=di%w#^jyka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-chijioke100-djangoproje-ron28sxq88l.ws-eu118.gitpod.io', '.herokuapp.com']
 
