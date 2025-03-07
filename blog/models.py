15e5ebd3d3bcc8301edabb9b90b629b392af2d5b
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2025-03-07 16:09:26 +0000

    implement comment model

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,12 @@ class Post(models.Model):
 
     #def __str__(self):
      #   return f"Ticket for {self.ticket_holder}"
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
