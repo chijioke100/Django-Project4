02d8bfab577ce2b201eef41ea892e578cf38b7b0
Chijioke Philip Ogbonnaya <chijiokee31@gmail.com>
2025-03-13 17:59:30 +0000

    add comment form

--- blog/views.py
+++ blog/views.py
@@ -37,6 +37,8 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
+
 
     return render(
         request,
@@ -44,6 +46,8 @@ def post_detail(request, slug):
         {"post": post,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
+        
         },
     )
 
